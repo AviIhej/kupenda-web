a97777133631dd2f3a837c1e9c1be25b71fbe2ec
Avi Ihej <aviihej@gmail.com>
2019-11-18 17:07:04 -0800

    form fix

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -1,35 +1,33 @@
-import React, { Component } from "react";
-import { Parallax } from "react-scroll-parallax";
-import "./Form.css";
+import React, { Component } from 'react';
+import { Parallax } from 'react-scroll-parallax';
+import './Form.css';
 
 const Form = () => {
-  return (
-    <div>
-<form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
-  <input type="hidden" name="bot-field" />
-  <input type="hidden" name="form-name" value="contact" />
-  <div className="field half first">
-    <label htmlFor="name">Name</label>
-    <input type="text" name="name" id="name" />
-  </div>
-  <div className="field half">
-    <label htmlFor="email">Email</label>
-    <input type="text" name="email" id="email" />
-  </div>
-  <div className="field">
-    <label htmlFor="message">Message</label>
-    <textarea name="message" id="message" rows="6" />
-  </div>
-  <ul className="actions">
-    <li>
-      <input type="submit" value="Send Message" className="special" />
-    </li>
-    <li>
-      <input type="reset" value="Clear" />
-    </li>
-  </ul>
-</form>
-    </div>
-  );
-};
-export default Form;
+  return(
+    <section>
+    <form name="contact" method="post" action="/mission" data-netlify="true" data-netlify-honeypot="bot-field">
+
+        <input type="hidden" name="bot-field" />
+
+        <div className="field half first">
+            <label htmlFor="name">Name</label>
+            <input type="text" name="name" id="name" required/>
+        </div>
+        <div className="field half">
+            <label htmlFor="email">Email</label>
+            <input type="text" name="email" id="email" required/>
+        </div>
+        <div className="field">
+            <label htmlFor="message">Message</label>
+            <textarea name="message" id="message" rows="6" required></textarea>
+        </div>
+        <ul className="actions">
+            <li><input type="submit" value="Send Message" className="special" /></li>
+            <li><input type="reset" value="Clear" /></li>
+        </ul>
+    </form>
+</section>
+    
+  )
+}
+export default Form
