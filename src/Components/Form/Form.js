bc3e59aab87cf82521d3021102a2682745ead9b2
Avi Ihej <aviihej@gmail.com>
2019-11-18 17:11:35 -0800

    form fix 2

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -2,32 +2,59 @@ import React, { Component } from 'react';
 import { Parallax } from 'react-scroll-parallax';
 import './Form.css';
 
-const Form = () => {
-  return(
-    <section>
-    <form name="contact" method="post" action="/mission" data-netlify="true" data-netlify-honeypot="bot-field">
-
-        <input type="hidden" name="bot-field" />
-
-        <div className="field half first">
-            <label htmlFor="name">Name</label>
-            <input type="text" name="name" id="name" required/>
-        </div>
-        <div className="field half">
-            <label htmlFor="email">Email</label>
-            <input type="text" name="email" id="email" required/>
-        </div>
-        <div className="field">
-            <label htmlFor="message">Message</label>
-            <textarea name="message" id="message" rows="6" required></textarea>
-        </div>
-        <ul className="actions">
-            <li><input type="submit" value="Send Message" className="special" /></li>
-            <li><input type="reset" value="Clear" /></li>
-        </ul>
-    </form>
-</section>
-    
-  )
+const encode = (data) => {
+  return Object.keys(data)
+      .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+      .join("&");
 }
+
+class Form extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { name: "", email: "", message: "" };
+  }
+
+  /* Here’s the juicy bit for posting the form submission */
+
+  handleSubmit = e => {
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({ "form-name": "contact", ...this.state })
+    })
+      .then(() => alert("Success!"))
+      .catch(error => alert(error));
+
+    e.preventDefault();
+  };
+
+  handleChange = e => this.setState({ [e.target.name]: e.target.value });
+
+  render() {
+    const { name, email, message } = this.state;
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <p>
+          <label>
+            Your Name: <input type="text" name="name" value={name} onChange={this.handleChange} />
+          </label>
+        </p>
+        <p>
+          <label>
+            Your Email: <input type="email" name="email" value={email} onChange={this.handleChange} />
+          </label>
+        </p>
+        <p>
+          <label>
+            Message: <textarea name="message" value={message} onChange={this.handleChange} />
+          </label>
+        </p>
+        <p>
+          <button type="submit">Send</button>
+        </p>
+      </form>
+    );
+  }
+}
+
 export default Form
